3bd496b1bdf8bac28d44bb5ba64339ae2853c21f
geofell <geofell22@gmail.com>
2019-10-25 18:34:15 -0400

    added Access-Control properites to address CORS

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var express = require("express");
 // =============================================================
 var app = express();
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
